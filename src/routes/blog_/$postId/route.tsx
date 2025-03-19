c238ba6e502ca237e0c261e4c0b0293685385f2e
Tetsuro Yamaguchi <1284712+mint-rabi@users.noreply.github.com>
2025-03-19 16:58:55 +0900

    fix bug

--- src/routes/blog_/$postId/route.tsx
+++ src/routes/blog_/$postId/route.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { fetch } from './loader.ts'
+import { fetch } from './-api/loader.ts'
 export const Route = createFileRoute('/blog_/$postId')({
   loader: ({ params }) => {
     return fetch(params.postId)
